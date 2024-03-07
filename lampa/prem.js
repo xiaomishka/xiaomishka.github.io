e21635d65b18b06c92f704bb922dfe215ac304b2
xiaomishka <78800190+xiaomishka@users.noreply.github.com>
2024-03-07 21:01:39 +0400

    Update prem.js

--- lampa/prem.js
+++ lampa/prem.js
@@ -1,39 +1,22 @@
 (function () {
-    'use strict';	
-   var garbage_collector = {
-      name: 'тестовое',
-      version: '1.0',
-      description: 'Тестовое'
+    'use strict';
+
+    var adBlocker = {
+        name: 'Блокировщик рекламы',
+        version: '1.0',
+        description: 'Отключает воспроизведение рекламы в кинотеатре'
     };
-		Lampa.Listener.follow('app',(e)=>{
-        if(e.type == 'ready'){
-			// Переопределяем функцию получения списка заблокированных адресов, чтобы она возвращала пустой массив
-network$6.silent = function(url, successCallback, errorCallback) {
-    // Просто вызываем successCallback с пустым массивом, игнорируя реальный запрос
-    successCallback([]);
-};
 
-// Переопределяем класс VideoBlock, чтобы он не воспроизводил рекламу
-class VideoBlock {
-    constructor(number) {
-        // Пустой конструктор
-    }
-
-    start() {
-        // Пустая функция, чтобы не начиналось воспроизведение
-    }
-
-    load(data) {
-        // Пустая функция, чтобы не загружалась реклама
-    }
-
-    create(data) {
-        // Пустая функция, чтобы не создавался блок рекламы
-    }
-}
-
-// Теперь скрипт будет игнорировать получение списка заблокированных адресов и воспроизведение рекламы
-alert('Активация'); 
+    Lampa.Listener.follow('app', function(e) {
+        if (e.type == 'ready') {
+            // Проверяем, загрузилась ли страница
+            var ads = document.querySelectorAll('.ad-video-block');
+            if (ads.length > 0) {
+                // Если есть блоки рекламы, удаляем их
+                ads.forEach(function(ad) {
+                    ad.remove();
+                });
+            }
         }
     });
 
