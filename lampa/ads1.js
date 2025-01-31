cb1419d4b346080643b4663431260d3506b378fb
xiaomishka <78800190+xiaomishka@users.noreply.github.com>
2025-02-01 01:25:30 +0400

    Update ads1.js

--- lampa/ads1.js
+++ lampa/ads1.js
@@ -1,13 +1,3 @@
-const observer = new MutationObserver(() => {
-    let adBlock = document.querySelector('.ad-preroll');
-    if (adBlock) {
-        clearInterval(this.timer);
-        this.video.pause();
-        this.video.src = '';
-        this.block.remove();
-        this.listener.send('ended');
-        observer.disconnect(); // Остановить наблюдение после удаления
-    }
-});
-
-observer.observe(document.body, { childList: true, subtree: true });
+var ru = {
+ ad: 'Никакой рекламы пацаны'
+}
