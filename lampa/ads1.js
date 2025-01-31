c5ca87f115ae47c265ea20e3f6a6ccf5d9cfc2dc
xiaomishka <78800190+xiaomishka@users.noreply.github.com>
2025-02-01 00:57:47 +0400

    Update ads1.js

--- lampa/ads1.js
+++ lampa/ads1.js
@@ -1,3 +1,5 @@
+(function () {
 VideoBlock.prototype.start = function () { console.log("Ad skipped!"); };
 VideoBlock.prototype.create = function () {};
 VideoBlock.prototype.load = function (data) { this.destroy(); };
+})();
