91f79bd9b31893c258f27f37833374db00afbe23
xiaomishka <78800190+xiaomishka@users.noreply.github.com>
2025-02-01 02:23:58 +0400

    Update ads1.js

--- lampa/ads1.js
+++ lampa/ads1.js
@@ -1,41 +1,8 @@
-(function () {
-    console.log("Принудительная замена рекламы!");
-
-    function enforceNoAds(target) {
-        return new Proxy(target, {
-            get(obj, prop) {
-                if (prop === "ad") {
-                    return "НЕ реклама";
-                }
-                return obj[prop];
-            },
-            set(obj, prop, value) {
-                if (prop === "ad") {
-                    console.log("Попытка изменить ad заблокирована!");
-                    return true; // Блокируем изменение
-                }
-                obj[prop] = value;
-                return true;
-            }
-        });
+setInterval(() => {
+    if (typeof user_data !== "undefined") {
+        user_data.premium = Date.now() + 1000 * 60 * 60 * 24 * 365 * 100;
     }
-
-    // Перехватываем уже существующие языковые объекты
-    if (window.ru) window.ru = enforceNoAds(window.ru);
-    if (window.en) window.en = enforceNoAds(window.en);
-    if (window.de) window.de = enforceNoAds(window.de);
-    if (window.fr) window.fr = enforceNoAds(window.fr);
-
-    // Отслеживаем добавление новых языков
-    new Proxy(window, {
-        set(target, prop, value) {
-            if (typeof value === "object" && value !== null) {
-                target[prop] = enforceNoAds(value);
-            } else {
-                target[prop] = value;
-            }
-            return true;
-        }
-    });
-
-})();
+    if (typeof Utils$2 !== "undefined") {
+        Utils$2.countDays = () => 9999;
+    }
+}, 1000);
