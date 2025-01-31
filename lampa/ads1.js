9748c91859cc6810c9b9202237d46d9e5b5566d5
xiaomishka <78800190+xiaomishka@users.noreply.github.com>
2025-02-01 01:44:24 +0400

    Update ads1.js

--- lampa/ads1.js
+++ lampa/ads1.js
@@ -1,7 +1,8 @@
-
-(function () {
- var ru = {
- ad: 'Никакой рекламы пацаны'
+if (window.ru && typeof window.ru === "object") {
+    Object.defineProperty(window.ru, 'ad', {
+        value: 'НЕ реклама',
+        writable: false, // Делаем неизменяемым (по желанию)
+        configurable: false, // Запрещаем повторное определение
+        enumerable: true
+    });
 }
-console.log("Подменили");
-})();
