deb627bc45a462daa5d761b10b8c88af67a2dc08
xiaomishka <78800190+xiaomishka@users.noreply.github.com>
2025-02-01 01:47:22 +0400

    Update ads1.js

--- lampa/ads1.js
+++ lampa/ads1.js
@@ -1,8 +1,36 @@
-if (window.ru && typeof window.ru === "object") {
-    Object.defineProperty(window.ru, 'ad', {
-        value: 'НЕ реклама',
-        writable: false, // Делаем неизменяемым (по желанию)
-        configurable: false, // Запрещаем повторное определение
-        enumerable: true
+(function () {
+    console.log("Переопределение рекламы");
+
+    function overrideAdProperty(langObj) {
+        if (langObj && typeof langObj === "object") {
+            Object.defineProperty(langObj, 'ad', {
+                get: function () {
+                    return "НЕ реклама";
+                },
+                set: function () {
+                    console.log("Попытка изменить ad заблокирована!");
+                },
+                configurable: false, 
+                enumerable: true
+            });
+        }
+    }
+
+    // Переопределяем свойство ad в уже существующих объектах
+    if (window.ru) overrideAdProperty(window.ru);
+    if (window.en) overrideAdProperty(window.en);
+    if (window.de) overrideAdProperty(window.de);
+    if (window.fr) overrideAdProperty(window.fr);
+
+    // Следим за добавлением новых языков
+    new Proxy(window, {
+        set(target, prop, value) {
+            if (typeof value === "object" && value !== null) {
+                overrideAdProperty(value);
+            }
+            target[prop] = value;
+            return true;
+        }
     });
-}
+
+})();
