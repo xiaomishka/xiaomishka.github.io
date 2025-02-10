ee089fb41d1c7f6c4c7db2777768880a5817c093
xiaomishka <78800190+xiaomishka@users.noreply.github.com>
2025-02-10 19:19:12 +0400

    Update ads1.js

--- lampa/ads1.js
+++ lampa/ads1.js
@@ -1,11 +1,22 @@
 (function() {
-    Object.defineProperty(ru, 'ad', {
-        get: function() {
-            return 'НЕ реклама';
-        },
-        set: function() {
-            console.warn('Попытка изменить ru.ad была заблокирована');
-        },
-        configurable: false
+    function replaceAdText() {
+        if (window.ru && window.ru.ad) {
+            window.ru.ad = 'Приятного чаяпития'; // Замените на нужное слово
+        }
+    }
+
+    // Первоначальная замена
+    replaceAdText();
+
+    // Интервал для периодической замены (каждые 500 мс)
+    setInterval(replaceAdText, 500);
+
+    // Используем MutationObserver для отслеживания изменений объекта window.ru
+    var observer = new MutationObserver(replaceAdText);
+
+    observer.observe(document.documentElement, {
+        childList: true,
+        subtree: true
     });
+
 })();
